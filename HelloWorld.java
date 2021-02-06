1c4bfc59a66cc8a3779fbbd736065dbeeae66aa4
Shivansh Bhatnagar <40287723+bhatnagarshivansh@users.noreply.github.com>
2021-02-06 17:16:17 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,8 @@ import java.util.*;
 public class HelloWorld {
   public static void main(String[] args) {
       System.out.println("Hello World");
+      Scanner kb=new Scanner(System.in);
+      int n=kb.nextInt();
+      System.out.println(n);
   }
   }
